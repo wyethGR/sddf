8edbf70e81ec88cbbd034167e51bfd1b5172edca
juliab <midnight@trainwit.ch>
2024-06-07 16:32:46 +1000

    basic usdhc reset + IRQ

--- drivers/usdhc/imx/include/usdhc.h
+++ drivers/usdhc/imx/include/usdhc.h
@@ -2,12 +2,13 @@
 
 #include <stdint.h>
 
-/* The driver is based on the i.MX 8M Mini Applications Processor Reference Manual Rev 3, 11/2020.
+#include <sddf/util/util.h>
+
+/* The driver is based on the i.MX8 Quad Applications Processor Reference Manual Rev 3.1, 06/2021.
    The following register descriptions and layout are from section 10.3.7.1.1.
 
     uSDHC1 base address: 30B4_0000h
     uSDHC2 base address: 30B5_0000h
-    uSDHC3 base address: 30B6_0000h
 */
 
 struct imx_usdhc_regs {
@@ -45,3 +46,58 @@ struct imx_usdhc_regs {
     uint32_t cqe; /* 100h Command Queue (CQE) ROZ */
 };
 typedef volatile struct imx_usdhc_regs imx_usdhc_regs_t;
+
+/* SYS_CTRL Bits. See 10.3.7.1.13 */
+#define USDHC_SYS_CTRL_RSTA  BIT(24)  /* Software reset for all */
+#define USDHC_SYS_CTRL_RSTC  BIT(25)  /* Software reset for CMD line */
+#define USDHC_SYS_CTRL_RSTD  BIT(26)  /* Software reset for data line */
+#define USDHC_SYS_CTRL_INITA BIT(27)  /* Initialization active */
+#define USDHC_SYS_CTRL_RSTT  BIT(28)  /* Reset tuning */
+
+/* INT_STATUS_EN Bits. See 10.3.7.1.15 */
+#define USDHC_INT_STATUS_CCSEN    BIT(0)   /* Command complete status enable */
+#define USDHC_INT_STATUS_TCSEN    BIT(1)   /* Transfer complete status enable */
+#define USDHC_INT_STATUS_BGESEN   BIT(2)   /* Block gap event status enable */
+#define USDHC_INT_STATUS_DINTSEN  BIT(3)   /* DMA interrupt status enable */
+#define USDHC_INT_STATUS_BWRSEN   BIT(4)   /* Buffer write ready status enable */
+#define USDHC_INT_STATUS_BRRSEN   BIT(5)   /* Buffer read ready status enable */
+#define USDHC_INT_STATUS_CINSSEN  BIT(6)   /* Card insertion status enable */
+#define USDHC_INT_STATUS_CRMSEN   BIT(7)   /* Card removal status enable */
+#define USDHC_INT_STATUS_CINTSEN  BIT(8)   /* Card interrupt status enable */
+#define USDHC_INT_STATUS_RTESEN   BIT(12)  /* Re-tuning event status enable */
+#define USDHC_INT_STATUS_TPSEN    BIT(13)  /* Tuning pass status enable */
+#define USDHC_INT_STATUS_CQISEN   BIT(14)  /* Command queuing status enable */
+#define USDHC_INT_STATUS_CTOESEN  BIT(16)  /* Command timeout error status enable */
+#define USDHC_INT_STATUS_CCESEN   BIT(17)  /* Command CRC error status enable */
+#define USDHC_INT_STATUS_CEBESEN  BIT(18)  /* Command end bit error status enable */
+#define USDHC_INT_STATUS_CIESEN   BIT(19)  /* Command indx error status enable */
+#define USDHC_INT_STATUS_DTOESEN  BIT(20)  /* Data timeout error status enable*/
+#define USDHC_INT_STATUS_DCSESEN  BIT(21)  /* Data CRC error status enable */
+#define USDHC_INT_STATUS_DEBESEN  BIT(22)  /* Data end bit error status enable */
+#define USDHC_INT_STATUS_AC12ESEN BIT(24)  /* Auto CMD12 error status enable */
+#define USDHC_INT_STATUS_TNESEN   BIT(26)  /* Tuning error status enable */
+#define USDHC_INT_STATUS_DMAESEN  BIT(28)  /* DMA error status enable */
+
+/* INT_SIGNAL_EN Bits. See 10.3.7.1.16 */
+#define USDHC_INT_SIGNAL_CCIEN    BIT(0)   /* Command complete interrupt enable */
+#define USDHC_INT_SIGNAL_TCIEN    BIT(1)   /* Transfer complete interrupt enable */
+#define USDHC_INT_SIGNAL_BGEIEN   BIT(2)   /* Block gap event interrupt enable */
+#define USDHC_INT_SIGNAL_DINTIEN  BIT(3)   /* DMA interrupt interrupt enable */
+#define USDHC_INT_SIGNAL_BWRIEN   BIT(4)   /* Buffer write ready interrupt enable */
+#define USDHC_INT_SIGNAL_BRRIEN   BIT(5)   /* Buffer read ready interrupt enable */
+#define USDHC_INT_SIGNAL_CINSIEN  BIT(6)   /* Card insertion interrupt enable */
+#define USDHC_INT_SIGNAL_CRMIEN   BIT(7)   /* Card removal interrupt enable */
+#define USDHC_INT_SIGNAL_CINTIEN  BIT(8)   /* Card interrupt enable */
+#define USDHC_INT_SIGNAL_RTEIEN   BIT(12)  /* Re-tuning event interrupt enable */
+#define USDHC_INT_SIGNAL_TPIEN    BIT(13)  /* Tuning pass interrupt enable */
+#define USDHC_INT_SIGNAL_CQIIEN   BIT(14)  /* Command queuing interrupt enable */
+#define USDHC_INT_SIGNAL_CTOEIEN  BIT(16)  /* Command timeout error interrupt enable */
+#define USDHC_INT_SIGNAL_CCEIEN   BIT(17)  /* Command CRC error interrupt enable */
+#define USDHC_INT_SIGNAL_CEBEIEN  BIT(18)  /* Command end bit error interrupt enable */
+#define USDHC_INT_SIGNAL_CIEIEN   BIT(19)  /* Command indx error interrupt enable */
+#define USDHC_INT_SIGNAL_DTOEIEN  BIT(20)  /* Data timeout error interrupt enable*/
+#define USDHC_INT_SIGNAL_DCSEIEN  BIT(21)  /* Data CRC error interrupt enable */
+#define USDHC_INT_SIGNAL_DEBEIEN  BIT(22)  /* Data end bit error interrupt enable */
+#define USDHC_INT_SIGNAL_AC12EIEN BIT(24)  /* Auto CMD12 error interrupt enable */
+#define USDHC_INT_SIGNAL_TNEIEN   BIT(26)  /* Tuning error interrupt enable */
+#define USDHC_INT_SIGNAL_DMAEIEN  BIT(28)  /* DMA error interrupt enable */
--- drivers/usdhc/imx/usdhc.c
+++ drivers/usdhc/imx/usdhc.c
@@ -3,19 +3,89 @@
 #include <microkit.h>
 #include <sddf/util/printf.h>
 
-imx_usdhc_regs_t *usdhc1_regs;
-imx_usdhc_regs_t *usdhc2_regs;
-imx_usdhc_regs_t *usdhc3_regs;
+imx_usdhc_regs_t *usdhc_regs;
 
 /*
     TODO: Details
         10.3.3.6.3 card interrupt handling.
 */
+void usdhc_notified(void) {
+    microkit_dbg_puts("usdhc IRQ!\n");
+
+    usdhc_regs->int_status_en |=
+      USDHC_INT_STATUS_CCSEN | USDHC_INT_STATUS_TCSEN |
+      USDHC_INT_STATUS_CIESEN | USDHC_INT_STATUS_CRMSEN |
+      USDHC_INT_STATUS_CTOESEN | USDHC_INT_STATUS_DTOESEN |
+      USDHC_INT_STATUS_CIESEN | USDHC_INT_STATUS_DCSESEN |
+      USDHC_INT_STATUS_CCESEN | USDHC_INT_STATUS_CINSSEN;
+
+    usdhc_regs->int_signal_en |=
+      USDHC_INT_SIGNAL_CCIEN | USDHC_INT_SIGNAL_TCIEN |
+      USDHC_INT_SIGNAL_CIEIEN | USDHC_INT_SIGNAL_CRMIEN |
+      USDHC_INT_SIGNAL_CTOEIEN | USDHC_INT_SIGNAL_DTOEIEN |
+      USDHC_INT_SIGNAL_CIEIEN | USDHC_INT_SIGNAL_DCSEIEN |
+      USDHC_INT_SIGNAL_CCEIEN | USDHC_INT_SIGNAL_CINSIEN;
+}
 
 void notified(microkit_channel ch) {
-    microkit_dbg_puts("notification!\n");
+    switch (ch) {
+        case 1:
+            usdhc_notified();
+            break;
+
+        default:
+            sddf_printf("notification on unknown channel: %d\n", ch);
+            break;
+    }
+
+    microkit_irq_ack(ch);
 }
+
+void usdhc_send_command(uint32_t cmd_arg /* , uint32_t cmd_index, args */) {
+    /* Ref: 10.3.4.1 */
+    uint32_t cmd_xfr_typ = 0;
+    // cmd_xfr_typ = (cmd_index & 0x3f) >> 24;
+    /* set CMDTYP, DPSEL, CICEN, CCCEN, RSTTYP, DTDSEL accorind to the command index; */
+    // if (iinternal DMA)
+    // if (multi-block transfer)
+
+    usdhc_regs->cmd_arg = cmd_arg;
+    usdhc_regs->cmd_xfr_typ = cmd_xfr_typ;
+
+    while (!(usdhc_regs->int_status & /* command complete bit */ BIT(0)));
+
+    /* check errors & clear CC / errors ??*/
+
+    usdhc_regs->int_status |= BIT(0); /* command complete field */
+}
+
+
+void usdhc_software_reset(void) {
+    /* Ref: See 10.3.4.2.2. */
+    usdhc_regs->sys_ctrl |= USDHC_SYS_CTRL_RSTA;
+    /* opt: set DTOCV and SDCLKFS and IO pad to 3.0V ??????*/
+    /* poll some bits?? */
+    usdhc_regs->sys_ctrl |= USDHC_SYS_CTRL_INITA; /* 80 clock ticks for power up */
+    usdhc_send_command(0);
+}
+
 void init() {
     microkit_dbg_puts("hello from usdhc driver\n");
-    sddf_printf("%d %d %d\n", usdhc1_regs->pres_state, usdhc2_regs->pres_state, usdhc3_regs->pres_state);
+    sddf_printf("%d\n", usdhc_regs->pres_state);
+
+    usdhc_regs->int_status_en |=
+      USDHC_INT_STATUS_CCSEN | USDHC_INT_STATUS_TCSEN |
+      USDHC_INT_STATUS_CIESEN | USDHC_INT_STATUS_CRMSEN |
+      USDHC_INT_STATUS_CTOESEN | USDHC_INT_STATUS_DTOESEN |
+      USDHC_INT_STATUS_CIESEN | USDHC_INT_STATUS_DCSESEN |
+      USDHC_INT_STATUS_CCESEN | USDHC_INT_STATUS_CINSSEN;
+
+    usdhc_regs->int_signal_en |=
+      USDHC_INT_SIGNAL_CCIEN | USDHC_INT_SIGNAL_TCIEN |
+      USDHC_INT_SIGNAL_CIEIEN | USDHC_INT_SIGNAL_CRMIEN |
+      USDHC_INT_SIGNAL_CTOEIEN | USDHC_INT_SIGNAL_DTOEIEN |
+      USDHC_INT_SIGNAL_CIEIEN | USDHC_INT_SIGNAL_DCSEIEN |
+      USDHC_INT_SIGNAL_CCEIEN | USDHC_INT_SIGNAL_CINSIEN;
+
+    usdhc_software_reset();
 }
