278a1e5c05a91f8137747a16bac7aedfd7fe7379
juliab <midnight@trainwit.ch>
2024-06-12 19:07:11 +1000

    attempt to setup iomuxc registers for usdhc
    .. it doesn't make a difference tho

--- drivers/usdhc/imx/usdhc.c
+++ drivers/usdhc/imx/usdhc.c
@@ -4,6 +4,7 @@
 #include <sddf/util/printf.h>
 
 imx_usdhc_regs_t *usdhc_regs;
+volatile uint32_t *iomuxc_regs;
 
 #define USDHC_INT_CHANNEL 1
 
@@ -121,8 +122,8 @@ void usdhc_setup_clock() {
 
     /*
      TODO: Clock driver! No exist
-     ... let's just assume linux's dts is right as uboot doesn't have a freq...
-     ... https://git.toradex.com/cgit/linux-toradex.git/tree/arch/arm64/boot/dts/freescale/imx8mq.dtsi?h=toradex_5.4-2.3.x-imx#n1144
+     ... let's just assume what i found elsewhere is right...
+     ... https://elixir.bootlin.com/linux/v6.9.3/source/arch/arm64/boot/dts/freescale/imx8mq.dtsi#L1499
      ... 0x400000000 => 400 MHz base clock...
 
      So (0x400_000_000 / (256 * 16)) == 0x400_000; divisor = 16, prescalar = 256
@@ -163,16 +164,77 @@ void usdhc_reset(void)
     usdhc_send_command_poll(USDHC_CMD_GO_IDLE_STATE);
 }
 
-void read_support_voltages() {
+void usdhc_read_support_voltages() {
     uint32_t host_cap = usdhc_regs->host_ctrl_cap;
     sddf_printf("host caps: %u\n", host_cap);
+}
+
+#define IOMUX_ALT0 0b000
+#define IOMUX_ALT1 0b001
+#define IOMUX_ALT5 0b101
+
+// value after reset, sane defaults
+#define IOMUX_PAD_CTL_PULLUP   BIT(6) /* defaults to pulldown. */
+
+/* Ref: Section 8.
+Also see 10.3.2 for settings? TODO: i have no idea if the pad ctl setitngs are correct
+*/
+void usdhc_setup_iomuxc() {
+    /*
+        Page 1316-7 ! -- pad mux registers
+    */
+    /* 8.2.5.12 */
+    *(iomuxc_regs + 0x40) = IOMUX_ALT5; /* USDHC1_CD_B : IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO06 */
+
+    /* 8.2.5.36 onwards; PAD mux registers... */
+    *(iomuxc_regs + 0xA0) = IOMUX_ALT0; /* USDHC1_CLK     : IOMUXC_SW_MUX_CTL_PAD_SD1_CLK */
+    *(iomuxc_regs + 0xA4) = IOMUX_ALT0; /* USDHC1_CMD     : IOMUXC_SW_MUX_CTL_PAD_SD1_CMD */
+    *(iomuxc_regs + 0xA8) = IOMUX_ALT0; /* USDHC1_DATA0   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA0 */
+    *(iomuxc_regs + 0xAC) = IOMUX_ALT0; /* USDHC1_DATA1   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA1 */
+    *(iomuxc_regs + 0xB0) = IOMUX_ALT0; /* USDHC1_DATA2   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA2 */
+    *(iomuxc_regs + 0xB4) = IOMUX_ALT0; /* USDHC1_DATA3   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA3 */
+    *(iomuxc_regs + 0xB8) = IOMUX_ALT0; /* USDHC1_DATA4   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA4 */
+    *(iomuxc_regs + 0xBC) = IOMUX_ALT0; /* USDHC1_DATA5   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA5 */
+    *(iomuxc_regs + 0xC0) = IOMUX_ALT0; /* USDHC1_DATA6   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA6 */
+    *(iomuxc_regs + 0xC4) = IOMUX_ALT0; /* USDHC1_DATA7   : IOMUXC_SW_MUX_CTL_PAD_SD1_DATA7 */
+    *(iomuxc_regs + 0xC8) = IOMUX_ALT0; /* USDHC1_RESET_B : IOMUXC_SW_MUX_CTL_PAD_SD1_RESET_B */
+    *(iomuxc_regs + 0xCC) = IOMUX_ALT0; /* USDHC1_STROBE  : IOMUXC_SW_MUX_CTL_PAD_SD1_STROBE */
+
+    /* 8.2.5.9 */
+    *(iomuxc_regs + 0x34) = IOMUX_ALT1; /* USDHC1_VSELECT : IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO03 */
+    *(iomuxc_regs + 0x44) = IOMUX_ALT5; /* USDHC1_WP      : IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO07 */
+
+    /*
+        Pad control registers; pg 1429, 8.2.5.190 onwrads
+    */
+   // tie low => card attached, otherwise, high = can card detect...
+    *(iomuxc_regs + 0x2A8) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_CD_B : IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO06 */
+
+    *(iomuxc_regs + 0x308) |= 0; /* USDHC1_CLK     : IOMUXC_SW_PAD_CTL_PAD_SD1_CLK */
+    *(iomuxc_regs + 0x30C) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_CMD     : IOMUXC_SW_PAD_CTL_PAD_SD1_CMD */
+    *(iomuxc_regs + 0x310) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA0   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA0 */
+    *(iomuxc_regs + 0x314) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA1   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA1 */
+    *(iomuxc_regs + 0x318) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA2   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA2 */
+    *(iomuxc_regs + 0x31C) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA3   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA3 */
+    *(iomuxc_regs + 0x320) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA4   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA4 */
+    *(iomuxc_regs + 0x324) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA5   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA5 */
+    *(iomuxc_regs + 0x328) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA6   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA6 */
+    *(iomuxc_regs + 0x32C) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_DATA7   : IOMUXC_SW_PAD_CTL_PAD_SD1_DATA7 */
+    *(iomuxc_regs + 0x330) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_RESET_B : IOMUXC_SW_PAD_CTL_PAD_SD1_RESET_B */ /* active low*/
+    *(iomuxc_regs + 0x334) |= 0; /* USDHC1_STROBE  : IOMUXC_SW_PAD_CTL_PAD_SD1_STROBE */
+
+    *(iomuxc_regs + 0x29C) |= 0; /* USDHC1_VSELECT : IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO03 */
+    // tied high for card write protect detect
+    *(iomuxc_regs + 0x2AC) |= IOMUX_PAD_CTL_PULLUP; /* USDHC1_WP : IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO07 */
 }
 
 void init()
 {
     microkit_dbg_puts("hello from usdhc driver\n");
-    // read_support_voltages();
+    // usdhc_read_support_voltages();
 
+    // this doesn't do anything...???? (maybe it does but it looks the same so)
+    usdhc_setup_iomuxc();
     usdhc_reset();
 
     if (usdhc_regs->pres_state & USDHC_PRES_STATE_CINST) {
