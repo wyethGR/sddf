cc9b39e69cc24e3562e49ee700495a7607e2684b
Wyeth Greenlaw Rollins <wyeth.gr@gmail.com>
2024-06-28 10:45:48 -0500

    chore: Strip MAC address multiplexing, format with clang-format

--- network/components/virt_rx.c
+++ network/components/virt_rx.c
@@ -1,13 +1,14 @@
+#include <microkit.h>
+#include <sddf/network/constants.h>
+#include <sddf/network/queue.h>
+#include <sddf/util/fence.h>
+#include <sddf/util/printf.h>
+#include <sddf/util/util.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include <microkit.h>
-#include <sddf/network/queue.h>
-#include <sddf/network/constants.h>
-#include <sddf/util/fence.h>
-#include <sddf/util/util.h>
-#include <sddf/util/printf.h>
-#include <ethernet_config.h>
+
+#include "config.h"
 
 /* Notification channels */
 #define DRIVER_CH 0
@@ -23,121 +24,100 @@ uintptr_t rx_active_virt;
 uintptr_t rx_buffer_data_region_vaddr;
 uintptr_t rx_buffer_data_region_paddr;
 
-typedef struct state {
-    net_queue_handle_t rx_queue_drv;
-    net_queue_handle_t rx_queue_clients[NUM_CLIENTS];
-    uint8_t mac_addrs[NUM_CLIENTS][ETH_HWADDR_LEN];
-} state_t;
+net_queue_handle_t rx_queue_drv;
+net_queue_handle_t rx_queue_client;
 
-state_t state;
-
-/* Boolean to indicate whether a packet has been enqueued into the driver's free queue during notification handling */
+/* Boolean to indicate whether a packet has been enqueued into the driver's free
+ * queue during notification handling */
 static bool notify_drv;
 
-/* Return the client ID if the Mac address is a match. */
-int get_client(struct ethernet_header * buffer)
-{
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        bool match = true;
-        for (int i = 0; (i < ETH_HWADDR_LEN) && match; i++) if (buffer->dest.addr[i] != state.mac_addrs[client][i]) match = false;
-        if (match) return client;
-    }
-    return -1;
-}
-
-void rx_return(void)
-{
-    bool reprocess = true;
-    bool notify_clients[NUM_CLIENTS] = {false};
+void rx_return(void) {
+    bool reprocess     = true;
+    bool notify_client = false;
     while (reprocess) {
-        while (!net_queue_empty_active(&state.rx_queue_drv)) {
+        while (!net_queue_empty_active(&rx_queue_drv)) {
             net_buff_desc_t buffer;
-            int err = net_dequeue_active(&state.rx_queue_drv, &buffer);
+            int err = net_dequeue_active(&rx_queue_drv, &buffer);
             assert(!err);
 
-            buffer.io_or_offset = buffer.io_or_offset - rx_buffer_data_region_paddr;
-            microkit_arm_vspace_data_invalidate(buffer.io_or_offset + rx_buffer_data_region_vaddr, buffer.io_or_offset + rx_buffer_data_region_vaddr + ROUND_UP(buffer.len, CONFIG_L1_CACHE_LINE_SIZE_BITS));
+            buffer.io_or_offset =
+                buffer.io_or_offset - rx_buffer_data_region_paddr;
+            microkit_arm_vspace_data_invalidate(
+                buffer.io_or_offset + rx_buffer_data_region_vaddr,
+                buffer.io_or_offset + rx_buffer_data_region_vaddr +
+                    ROUND_UP(buffer.len, CONFIG_L1_CACHE_LINE_SIZE_BITS));
 
-            int client = get_client((struct ethernet_header *) (buffer.io_or_offset + rx_buffer_data_region_vaddr));
-            if (client >= 0) {
-                err = net_enqueue_active(&state.rx_queue_clients[client], buffer);
-                assert(!err);
-                notify_clients[client] = true;
-            } else {
-                buffer.io_or_offset = buffer.io_or_offset + rx_buffer_data_region_paddr;
-                err = net_enqueue_free(&state.rx_queue_drv, buffer);
-                assert(!err);
-                notify_drv = true;
-            }
+            err = net_enqueue_active(&rx_queue_client, buffer);
+            assert(!err);
+            notify_client = true;
         }
 
-        net_request_signal_active(&state.rx_queue_drv);
+        net_request_signal_active(&rx_queue_drv);
         reprocess = false;
 
-        if (!net_queue_empty_active(&state.rx_queue_drv)) {
-            net_cancel_signal_active(&state.rx_queue_drv);
+        if (!net_queue_empty_active(&rx_queue_drv)) {
+            net_cancel_signal_active(&rx_queue_drv);
             reprocess = true;
         }
     }
 
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        if (notify_clients[client] && net_require_signal_active(&state.rx_queue_clients[client])) {
-            net_cancel_signal_active(&state.rx_queue_clients[client]);
-            microkit_notify(client + CLIENT_CH);
-        }
-    }    
+    if (notify_client && net_require_signal_active(&rx_queue_client)) {
+        net_cancel_signal_active(&rx_queue_client);
+        microkit_notify(CLIENT_CH);
+    }
 }
 
-void rx_provide(void)
-{
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        bool reprocess = true;
-        while (reprocess) {
-            while (!net_queue_empty_free(&state.rx_queue_clients[client])) {
-                net_buff_desc_t buffer;
-                int err = net_dequeue_free(&state.rx_queue_clients[client], &buffer);
-                assert(!err);
-                assert(!(buffer.io_or_offset % NET_BUFFER_SIZE) && 
-                       (buffer.io_or_offset < NET_BUFFER_SIZE * state.rx_queue_clients[client].size));
+void rx_provide(void) {
+    bool reprocess = true;
+    while (reprocess) {
+        while (!net_queue_empty_free(&rx_queue_client)) {
+            net_buff_desc_t buffer;
+            int err = net_dequeue_free(&rx_queue_client, &buffer);
+            assert(!err);
+            assert(
+                !(buffer.io_or_offset % NET_BUFFER_SIZE) &&
+                (buffer.io_or_offset < NET_BUFFER_SIZE * rx_queue_client.size));
 
-                buffer.io_or_offset = buffer.io_or_offset + rx_buffer_data_region_paddr;
-                err = net_enqueue_free(&state.rx_queue_drv, buffer);
-                assert(!err);
-                notify_drv = true;
-            }
+            buffer.io_or_offset =
+                buffer.io_or_offset + rx_buffer_data_region_paddr;
+            err = net_enqueue_free(&rx_queue_drv, buffer);
+            assert(!err);
+            notify_drv = true;
+        }
 
-            net_request_signal_free(&state.rx_queue_clients[client]);
-            reprocess = false;
+        net_request_signal_free(&rx_queue_client);
+        reprocess = false;
 
-            if (!net_queue_empty_free(&state.rx_queue_clients[client])) {
-                net_cancel_signal_free(&state.rx_queue_clients[client]);
-                reprocess = true;
-            }
+        if (!net_queue_empty_free(&rx_queue_client)) {
+            net_cancel_signal_free(&rx_queue_client);
+            reprocess = true;
         }
     }
 
-    if (notify_drv && net_require_signal_free(&state.rx_queue_drv)) {
-        net_cancel_signal_free(&state.rx_queue_drv);
+    if (notify_drv && net_require_signal_free(&rx_queue_drv)) {
+        net_cancel_signal_free(&rx_queue_drv);
         microkit_notify_delayed(DRIVER_CH);
     }
 }
 
-void notified(microkit_channel ch)
-{
+void notified(microkit_channel ch) {
     rx_return();
     rx_provide();
 }
 
-void init(void)
-{
-    virt_mac_addr_init_sys(microkit_name, (uint8_t *) state.mac_addrs);
+void init(void) {
+    net_queue_init(&rx_queue_drv,
+                   (net_queue_t *) rx_free_drv,
+                   (net_queue_t *) rx_active_drv,
+                   BUFS_PER_DIR);
+    net_queue_init(&rx_queue_client,
+                   (net_queue_t *) rx_free_virt,
+                   (net_queue_t *) rx_active_virt,
+                   BUFS_PER_DIR);
+    net_buffers_init(&rx_queue_drv, rx_buffer_data_region_paddr);
 
-    net_queue_init(&state.rx_queue_drv, (net_queue_t *)rx_free_drv, (net_queue_t *)rx_active_drv, RX_QUEUE_SIZE_DRIV);
-    net_queue_init(&state.rx_queue_clients[0], (net_queue_t *)rx_free_virt, (net_queue_t *)rx_active_virt, RX_QUEUE_SIZE_CLI0);
-    net_buffers_init(&state.rx_queue_drv, rx_buffer_data_region_paddr);
-
-    if (net_require_signal_free(&state.rx_queue_drv)) {
-        net_cancel_signal_free(&state.rx_queue_drv);
+    if (net_require_signal_free(&rx_queue_drv)) {
+        net_cancel_signal_free(&rx_queue_drv);
         microkit_notify_delayed(DRIVER_CH);
     }
 }
--- network/components/virt_tx.c
+++ network/components/virt_tx.c
@@ -2,11 +2,12 @@
 #include <sddf/network/queue.h>
 #include <sddf/util/cache.h>
 #include <sddf/util/fence.h>
-#include <sddf/util/util.h>
 #include <sddf/util/printf.h>
-#include <ethernet_config.h>
+#include <sddf/util/util.h>
 
-#define DRIVER 0
+#include "config.h"
+
+#define DRIVER    0
 #define CLIENT_CH 1
 
 uintptr_t tx_free_drv;
@@ -17,117 +18,97 @@ uintptr_t tx_active_virt;
 uintptr_t tx_buffer_data_region_vaddr;
 uintptr_t tx_buffer_data_region_paddr;
 
-typedef struct state {
-    net_queue_handle_t tx_queue_drv;
-    net_queue_handle_t tx_queue_clients[NUM_CLIENTS];
-    uintptr_t buffer_region_vaddrs[NUM_CLIENTS];
-    uintptr_t buffer_region_paddrs[NUM_CLIENTS];
-} state_t;
+net_queue_handle_t tx_queue_drv;
+net_queue_handle_t tx_queue_client;
 
-state_t state;
-
-int extract_offset(uintptr_t *phys) {
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        if (*phys >= state.buffer_region_paddrs[client] && 
-            *phys < state.buffer_region_paddrs[client] + state.tx_queue_clients[client].size * NET_BUFFER_SIZE) {
-            *phys = *phys - state.buffer_region_paddrs[client];
-            return client;
-        }
-    }
-    return -1;
-}
-
-void tx_provide(void)
-{
-    bool enqueued = false;
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        bool reprocess = true;
-        while (reprocess) {
-            while (!net_queue_empty_active(&state.tx_queue_clients[client])) {
-                net_buff_desc_t buffer;
-                int err = net_dequeue_active(&state.tx_queue_clients[client], &buffer);
-                assert(!err);
-
-                if (buffer.io_or_offset % NET_BUFFER_SIZE || 
-                    buffer.io_or_offset >= NET_BUFFER_SIZE * state.tx_queue_clients[client].size) {
-                    sddf_dprintf("VIRT_TX|LOG: Client provided offset %lx which is not buffer aligned or outside of buffer region\n", buffer.io_or_offset);
-                    err = net_enqueue_free(&state.tx_queue_clients[client], buffer);
-                    assert(!err);
-                    continue;
-                }
-
-                cache_clean(buffer.io_or_offset + state.buffer_region_vaddrs[client], buffer.io_or_offset + state.buffer_region_vaddrs[client] + buffer.len);
-
-                buffer.io_or_offset = buffer.io_or_offset + state.buffer_region_paddrs[client];
-                err = net_enqueue_active(&state.tx_queue_drv, buffer);
-                assert(!err);
-                enqueued = true;
-            }
-
-            net_request_signal_active(&state.tx_queue_clients[client]);
-            reprocess = false;
-
-            if (!net_queue_empty_active(&state.tx_queue_clients[client])) {
-                net_cancel_signal_active(&state.tx_queue_clients[client]);
-                reprocess = true;
-            }
-        }
-    }
-
-    if (enqueued && net_require_signal_active(&state.tx_queue_drv)) {
-        net_cancel_signal_active(&state.tx_queue_drv);
-        microkit_notify_delayed(DRIVER);
-    }
-}
-
-void tx_return(void)
-{
+void tx_provide(void) {
+    bool enqueued  = false;
     bool reprocess = true;
-    bool notify_clients[NUM_CLIENTS] = {false};
     while (reprocess) {
-        while (!net_queue_empty_free(&state.tx_queue_drv)) {
+        while (!net_queue_empty_active(&tx_queue_client)) {
             net_buff_desc_t buffer;
-            int err = net_dequeue_free(&state.tx_queue_drv, &buffer);
+            int err = net_dequeue_active(&tx_queue_client, &buffer);
             assert(!err);
 
-            int client = extract_offset(&buffer.io_or_offset);
-            assert(client >= 0);
+            if (buffer.io_or_offset % NET_BUFFER_SIZE ||
+                buffer.io_or_offset >= NET_BUFFER_SIZE * tx_queue_client.size) {
+                sddf_dprintf("VIRT_TX|LOG: Client provided offset %lx which is "
+                             "not buffer aligned or outside of buffer region\n",
+                             buffer.io_or_offset);
+                err = net_enqueue_free(&tx_queue_client, buffer);
+                assert(!err);
+                continue;
+            }
 
-            err = net_enqueue_free(&state.tx_queue_clients[client], buffer);
+            cache_clean(buffer.io_or_offset + tx_buffer_data_region_vaddr,
+                        buffer.io_or_offset + tx_buffer_data_region_vaddr +
+                            buffer.len);
+
+            buffer.io_or_offset =
+                buffer.io_or_offset + tx_buffer_data_region_paddr;
+            err = net_enqueue_active(&tx_queue_drv, buffer);
             assert(!err);
-            notify_clients[client] = true;
+            enqueued = true;
         }
 
-        net_request_signal_free(&state.tx_queue_drv);
+        net_request_signal_active(&tx_queue_client);
         reprocess = false;
 
-        if (!net_queue_empty_free(&state.tx_queue_drv)) {
-            net_cancel_signal_free(&state.tx_queue_drv);
+        if (!net_queue_empty_active(&tx_queue_client)) {
+            net_cancel_signal_active(&tx_queue_client);
             reprocess = true;
         }
     }
 
-    for (int client = 0; client < NUM_CLIENTS; client++) {
-        if (notify_clients[client] && net_require_signal_free(&state.tx_queue_clients[client])) {
-            net_cancel_signal_free(&state.tx_queue_clients[client]);
-            microkit_notify(client + CLIENT_CH);
-        }
+    if (enqueued && net_require_signal_active(&tx_queue_drv)) {
+        net_cancel_signal_active(&tx_queue_drv);
+        microkit_notify_delayed(DRIVER);
     }
 }
 
-void notified(microkit_channel ch)
-{
+void tx_return(void) {
+    bool reprocess     = true;
+    bool notify_client = false;
+    while (reprocess) {
+        while (!net_queue_empty_free(&tx_queue_drv)) {
+            net_buff_desc_t buffer;
+            int err = net_dequeue_free(&tx_queue_drv, &buffer);
+            assert(!err);
+
+            err = net_enqueue_free(&tx_queue_client, buffer);
+            assert(!err);
+            notify_client = true;
+        }
+
+        net_request_signal_free(&tx_queue_drv);
+        reprocess = false;
+
+        if (!net_queue_empty_free(&tx_queue_drv)) {
+            net_cancel_signal_free(&tx_queue_drv);
+            reprocess = true;
+        }
+    }
+
+    if (notify_client && net_require_signal_free(&tx_queue_client)) {
+        net_cancel_signal_free(&tx_queue_client);
+        microkit_notify(CLIENT_CH);
+    }
+}
+
+void notified(microkit_channel ch) {
     tx_return();
     tx_provide();
 }
 
-void init(void)
-{
-    net_queue_init(&state.tx_queue_drv, (net_queue_t *)tx_free_drv, (net_queue_t *)tx_active_drv, TX_QUEUE_SIZE_DRIV);
-    net_queue_init(&state.tx_queue_clients[0], (net_queue_t *)tx_free_virt, (net_queue_t *)tx_active_virt, TX_QUEUE_SIZE_CLI0);
+void init(void) {
+    net_queue_init(&tx_queue_drv,
+                   (net_queue_t *) tx_free_drv,
+                   (net_queue_t *) tx_active_drv,
+                   BUFS_PER_DIR);
+    net_queue_init(&tx_queue_client,
+                   (net_queue_t *) tx_free_virt,
+                   (net_queue_t *) tx_active_virt,
+                   BUFS_PER_DIR);
 
-    state.buffer_region_vaddrs[0] = tx_buffer_data_region_vaddr;
-    state.buffer_region_paddrs[0] = tx_buffer_data_region_paddr;
-    
     tx_provide();
 }
